1e209155bfa75cbca355ab89ab12a305bd188acd
JayKowski <jaymaina5@gmail.org>
2019-11-04 21:14:50 +0300

    added my_each_with_index method

--- enumerables.rb
+++ enumerables.rb
@@ -4,10 +4,13 @@
 module Enumerables
   def my_each
     i = 0
-    while i < self.size
-      yield(self[i])
-      i += 1
-    end
+    while i < self.size do yield(self[i]); i += 1 end
   end
+
+  def my_each_with_index
+    i = 0
+    while i < self.size do yield(self[i], i) ; i += 1 end
+  end
+
   ############
 end
